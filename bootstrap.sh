ab3a564a3161917e815b2ce48e08c6fc77e7c529
Raphael Varonos <r.varonos@agneta.io>
2018-09-14 11:48:21 +0300

    Build file

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,10 @@ npm config set cache /home/agneta/.cache/npm --global
 npm config set package-lock false
 
 export HOME=/home/agneta
-export USER=agneta
-export USER_ID=1000
-export GROUP_ID=1000
+export NAME=agneta
+export USER_ID=2000
+export GROUP_ID=2000
 
-USER_ID=${USER_ID:-9001}
-GROUP_ID=${GROUP_ID:-9001}
-GROUP_NAME=$(getent group $GROUP_ID | cut -d: -f1)
-
-deluser --remove-home node
-if [ -z GROUP_NAME ]
-then
-addgroup -S -g $GROUP_ID $USER
-else
-groupmod -n $USER $GROUP_NAME
-fi
-adduser -S -u $USER_ID -G $USER $USER
-chown -R $USER:$USER $HOME
+addgroup -S -g $GROUP_ID $NAME
+adduser -S -u $USER_ID -G $NAME $NAME
+chown -R $NAME:$NAME $HOME
